feb3f6c0f290e6c365e7cbf730f2c2fba76a1197
Emma Holtzman <holtzmaner@gmail.com>
2024-07-03 13:41:47 -0400

    Debugging

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-[setup]
+from setuptools import setup
 
+setup(
+    name='occlab',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version<"3.10"',
+    ],
+)
 setup(
     package_dir={"":"src"}
 )
